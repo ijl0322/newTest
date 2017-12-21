09ef11754cd56c9ee83d06e73942a46e3d46cb0a
ijl0322 <ijl0322@gmail.com>
2017-12-21 16:19:46 -0500

    update framework

---
+++ newTest/newTest/newTest.h
@@ -0,0 +1,19 @@
+//
+//  newTest.h
+//  newTest
+//
+//  Created by Isabel Lee on 12/21/17.
+//  Copyright © 2017 Isabel Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for newTest.
+FOUNDATION_EXPORT double newTestVersionNumber;
+
+//! Project version string for newTest.
+FOUNDATION_EXPORT const unsigned char newTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <newTest/PublicHeader.h>
+
+
---
+++ newTest/newTest/newTest.swift
@@ -0,0 +1,15 @@
+//
+//  newTest.swift
+//  newTest
+//
+//  Created by Isabel Lee on 12/21/17.
+//  Copyright © 2017 Isabel Lee. All rights reserved.
+//
+
+import Foundation
+
+open class newTest {
+    public func sayHi() {
+        print("Hi!")
+    }
+}
